e4d97468f4b9261e8efc1e88f516122f7fc53faa
lethyadon <sean_b_keenan@hotmail.co.uk>
2025-06-04 23:41:16 +0100

    Add files via upload

---
+++ dashboard.js
@@ -0,0 +1,9 @@
+
+export default function Dashboard() {
+  return (
+    <main style={{ padding: '2rem', fontFamily: 'sans-serif' }}>
+      <h1>Dashboard</h1>
+      <p>This is your dashboard view. More functionality coming soon.</p>
+    </main>
+  );
+}
