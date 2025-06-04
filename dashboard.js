6998de26bf1774b02ffe01c9a0ab2c37e163c484
lethyadon <sean_b_keenan@hotmail.co.uk>
2025-06-05 00:01:28 +0100

    Delete dashboard.js

--- dashboard.js
+++
@@ -1,9 +0,0 @@
-
-export default function Dashboard() {
-  return (
-    <main style={{ padding: '2rem', fontFamily: 'sans-serif' }}>
-      <h1>Dashboard</h1>
-      <p>This is your dashboard view. More functionality coming soon.</p>
-    </main>
-  );
-}
