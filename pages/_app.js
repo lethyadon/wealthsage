8a5944e29e1a38255128a90cec2afc7cd5da34b0
lethyadon <sean_b_keenan@hotmail.co.uk>
2025-06-07 16:31:37 +0100

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,3 @@
-// pages/_app.js
 import '../styles/globals.css';
 import Head from 'next/head';
 
@@ -9,7 +8,6 @@ function MyApp({ Component, pageProps }) {
         <title>Wealth Sage</title>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta name="description" content="Your AI financial assistant." />
-        <link rel="icon" href="/favicon.ico" />
       </Head>
       <Component {...pageProps} />
     </>
